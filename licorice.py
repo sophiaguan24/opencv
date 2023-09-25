6e43ca75cc7b765ce5fdee9e5fc78ed3b971c296
sophiaguan24 <103451956+sophiaguan24@users.noreply.github.com>
2023-09-24 19:39:39 -0700

    speed increase + screenshots

--- licorice.py
+++ licorice.py
@@ -1,8 +1,9 @@
-import cv2
 from imutils import face_utils
 import dlib
-import random
 import numpy as np
+import cv2
+import random
+
 
 p = "shape_predictor_68_face_landmarks.dat"
 detector = dlib.get_frontal_face_detector()
@@ -11,25 +12,7 @@ predictor = dlib.shape_predictor(p)
 cap = cv2.VideoCapture(1)
 cap.set(3, 640)
 cap.set(4, 480)
-
-def catch(myPoints, mouth, open):
-    if (mouth[1][0] + 10 >= myPoints[0][0] and mouth[5][0] - 10 <= myPoints[1][0]) and (myPoints[1][1] + 4 >= mouth[5][1] and myPoints[0][1] - 8 <= mouth[5][1]) and open:
-        print("caught")
-        return True 
-    return False
-
-
-def moveDown(myPoints):
-    newP = list(myPoints)
-    newP[0] = list(newP[0])
-    newP[1] = list(newP[1])
-    newP[0][1] += 3
-    newP[1][1] += 3
-    newP[0] = tuple(newP[0])
-    newP[1] = tuple(newP[1])
-    newP = tuple(newP)
-    return newP
-
+count = 0
 def generateGummyBear():
     spx = random.randrange(100, 520)
     sp = (spx, 0)
@@ -38,18 +21,37 @@ def generateGummyBear():
     myPoints = (sp, ep, jp)
     return myPoints
 
+def catch(myPoints, mouth, open):
+ 
+    if (mouth[1][0] + 10 >= myPoints[0][0] and mouth[5][0] - 10 <= myPoints[1][0]) and (myPoints[1][1] + 4 >= mouth[5][1] and myPoints[0][1] - 8 <= mouth[5][1]) and open:
+        # print("caught")
+        return True 
+    return False
+
+def moveDown(myPoints, count):
+    newP = list(myPoints)
+    newP[0] = list(newP[0])
+    newP[1] = list(newP[1])
+    num = 3 + int(count/100)
+    newP[0][1] += num
+    newP[1][1] += num
+    newP[0] = tuple(newP[0])
+    newP[1] = tuple(newP[1])
+    newP = tuple(newP)
+    return newP
+
 start_point = (240,0)
 end_point = (255,25)
 myPoints = (start_point, end_point, "a")
-count = 0
-points = 0
 gummies = []
 open = False
+points = 0
+frames = []
 while True:
     _, image = cap.read()
 
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    # print(face_utils.FACIAL_LANDMARKS_IDXS)
+    #print(face_utils.FACIAL_LANDMARKS_IDXS)
 
     rects = detector(gray, 0)
     arr = []
@@ -57,9 +59,9 @@ while True:
 
         shape = predictor(gray, rect)
         shape = face_utils.shape_to_np(shape)
-        # print(shape)
-        for (x, y) in shape:
-            cv2.circle(image, (x, y), 2, (0, 255, 0), -1)
+        #print(shape)
+        # for (x, y) in shape:
+        #     cv2.circle(image, (x, y), 2, (0, 255, 0), -1)
         
         arr = shape[60:]
     
@@ -71,16 +73,23 @@ while True:
         else:
             open = True
             # print("opened")
-    count += 1
-    lose = False
+
+
+    #!---- undo ----
+
     if (count % 40 == 0):
         myPoints = generateGummyBear()
         gummies.append(myPoints)
         color = (0, 0, 0)
-
+    if (count % 100 == 0):
+        ret, frame = cap.read()
+        frames.append((ret,frame))
+        
+    count += 1
+    lose = False
     gummies2 = []
     for gummy in gummies:
-        gummy = moveDown(gummy)
+        gummy = moveDown(gummy, count)
         add = True
         if (len(arr) >= 8):
             # print("a")
@@ -88,20 +97,22 @@ while True:
             if caught:
                 add = False
                 points += 1
+                # print("point")
         if gummy[0][1] <= 480 and add:   
             gummies2.append(gummy)
         elif gummy[0][1] >= 480:
             lose = True
             break
+
         cv2.rectangle(image, gummy[0], gummy[1], color, -1)
-    
     pointText = "Points: " + str(points)
     image = cv2.flip(image, 1)
     cv2.putText(image, pointText, (0, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 1, cv2.LINE_AA)
-
+    # cv2.rectangle(image, myPoints[0], myPoints[1], color, -1)
     gummies = gummies2
-
+    
     cv2.imshow("Output", image)
+    
     if lose:
         # print("lost")
         ret, frame = cap.read()
@@ -116,10 +127,19 @@ while True:
                 cv2.imshow("frame", cv2.merge([zeros, zeros, R]))
                 if cv2.waitKey(1) & 0xFF == ord(' '):
                     break
+                # cv2.waitKey(0)
+                #break
+        num = 0
+        # print(len(frames))
+        for f in frames:
+            # print("catch")
+            num += 1
+            if f[0]:
+                cv2.imshow("frame" + str(num), f[1])
+            cv2.waitKey(0)
     
     if cv2.waitKey(1) & 0xFF == ord(' '):
         break
 
 cv2.destroyAllWindows()
 cap.release()
-
